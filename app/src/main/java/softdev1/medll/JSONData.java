04581527e28b284deb14f4ba5b5e47b61fbd4dcb
goalgitter <tarek_skouti@yahoo.de>
2016-01-14 07:17:44 +0100

    deleted:    .idea/codeStyleSettings.xml 	new file:   .idea/encodings.xml 	modified:   .idea/libraries/appcompat_v7_23_1_1.xml 	modified:   .idea/libraries/design_23_1_1.xml 	modified:   .idea/libraries/recyclerview_v7_23_1_1.xml 	modified:   .idea/libraries/support_annotations_23_1_1.xml 	modified:   .idea/libraries/support_v4_23_1_1.xml 	modified:   .idea/misc.xml 	modified:   .idea/workspace.xml 	new file:   app/src/main/java/softdev1/medll/ExpandableListAdapter.java 	new file:   app/src/main/java/softdev1/medll/JSONData.java 	modified:   app/src/main/java/softdev1/medll/LeitlinieMain.java 	modified:   app/src/main/res/layout/activity_leitlinie_main.xml 	new file:   app/src/main/res/layout/expandable_list.xml 	new file:   app/src/main/res/layout/expandable_list_group.xml 	new file:   app/src/main/res/layout/expandable_list_textitem.xml 	new file:   app/src/main/res/xml-entwurf-fachmodell.xml

---
+++ app/src/main/java/softdev1/medll/JSONData.java
@@ -0,0 +1,93 @@
+package softdev1.medll;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+/**
+ * Created by Tyrael on 09.01.2016.
+ */
+public class JSONData {
+    private ArrayList<String> kapitelName;
+
+    public JSONData(){
+        kapitelName = new ArrayList<>();
+    }
+
+    public ArrayList<String> getKapitelName(){
+       return this.kapitelName;
+    }
+
+    public void readXML() {
+
+        try {
+
+            File file = new File("/Users/Tyrael/Documents/GitHub/MedLL/app/src/main/res/xml-entwurf-fachmodell.xml");
+
+            DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance()
+                    .newDocumentBuilder();
+
+            Document doc = dBuilder.parse(file);
+
+            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+            kapitelName.add(doc.getDocumentElement().getNodeName().toString());
+            if (doc.hasChildNodes()) {
+
+                printNote(doc.getChildNodes());
+
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    private static void printNote(NodeList nodeList) {
+
+        for (int count = 0; count < nodeList.getLength(); count++) {
+
+            Node tempNode = nodeList.item(count);
+
+            // make sure it's element node.
+            if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                // get node name and value
+                System.out.println("\nNode Name =" + tempNode.getNodeName() + " [OPEN]");
+                System.out.println("Node Value =" + tempNode.getTextContent());
+
+                if (tempNode.hasAttributes()) {
+
+                    // get attributes names and values
+                    NamedNodeMap nodeMap = tempNode.getAttributes();
+
+                    for (int i = 0; i < nodeMap.getLength(); i++) {
+
+                        Node node = nodeMap.item(i);
+                        System.out.println("attr name : " + node.getNodeName());
+                        System.out.println("attr value : " + node.getNodeValue());
+
+                    }
+
+                }
+
+                if (tempNode.hasChildNodes()) {
+
+                    // loop again if has child nodes
+                    printNote(tempNode.getChildNodes());
+
+                }
+
+                System.out.println("Node Name =" + tempNode.getNodeName() + " [CLOSE]");
+
+            }
+
+        }
+
+    }
+
+}
--- app/src/main/java/softdev1/medll/LeitlinieMain.java
+++ app/src/main/java/softdev1/medll/LeitlinieMain.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ExpandableListView;
 import android.widget.Toast;
 
 import com.squareup.okhttp.Call;
@@ -23,14 +24,25 @@ import com.squareup.okhttp.Response;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class LeitlinieMain extends AppCompatActivity {
     private Button mToolsButton;
     public static final String TAG = LeitlinieMain.class.getSimpleName();
     private Leitlinie mLeitlinie;
-
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+    private XmlPullParserFactory xmlFactoryObject;
+    private XmlPullParser myparser;
 
 
     @Override
@@ -42,39 +54,42 @@ public class LeitlinieMain extends AppCompatActivity {
 
         String leitlinieUrl = "http://10.0.3.2/test.json";
         if (isNetworkAvailable()){
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url(leitlinieUrl).build();
-        Call call = client.newCall(request);
-        //execute by putting into queue
-        //asynchronous task
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Request request, IOException e) {
-                Log.v(TAG, "FAILURE");
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(leitlinieUrl).build();
+            Call call = client.newCall(request);
+            //execute by putting into queue
+            //asynchronous task
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
+                    Log.v(TAG, "FAILURE");
 
-            }
+                }
 
-            @Override
-            public void onResponse(Response response) throws IOException {
+                @Override
+                public void onResponse(Response response) throws IOException {
 
-                try {
-                    String jsonData = response.body().string();
-                    Log.v(TAG,jsonData );
-                    if (response.isSuccessful())  {
-                        mLeitlinie = parseLeitlinieData(jsonData);
+                    try {
+                        String jsonData = response.body().string();
+                        Log.v(TAG,jsonData );
+                        if (response.isSuccessful())  {
+                            mLeitlinie = parseLeitlinieData(jsonData);
 
-                    } else {
-                        alertUserAboutError();
+                        } else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught", e);
                     }
-                } catch (IOException e) {
-                    Log.e(TAG, "Exception caught", e);
-                }
-                catch(JSONException e){
-                    Log.e(TAG, "Exception caught", e);
-                }
+                    catch(JSONException e){
+                        Log.e(TAG, "Exception caught", e);
+                    }
+                    catch(Exception e){
+                        Log.e(TAG, "Exception caught", e);
+                    }
 
-            }
-        });}
+                }
+            });}
         else {
             Toast.makeText(this,"Network is unavailable",Toast.LENGTH_LONG).show();
         }
@@ -91,6 +106,73 @@ public class LeitlinieMain extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.expandableListView);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+        // Listview Group click listener
+        expListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                // Toast.makeText(getApplicationContext(),
+                // "Group Clicked " + listDataHeader.get(groupPosition),
+                // Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Expanded",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Listview Group collasped listener
+        expListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        listDataHeader.get(groupPosition) + " Collapsed",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(
+                        getApplicationContext(),
+                        listDataHeader.get(groupPosition)
+                                + " : "
+                                + listDataChild.get(
+                                listDataHeader.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+        });
+
 
     }
 
@@ -184,7 +266,7 @@ public class LeitlinieMain extends AppCompatActivity {
 
     private void alertUserAboutError() {
     AlertDialogFragment dialog = new AlertDialogFragment();
-        dialog.show(getFragmentManager(),"error_dialog");
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 
     @Override
@@ -208,6 +290,54 @@ public class LeitlinieMain extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+
+        // Adding child data
+        JSONData jsonData = new JSONData();
+        jsonData.readXML();
+        ArrayList jsonDataHeader = new ArrayList<String>();
+
+
+        listDataHeader.add("Top 250");
+        listDataHeader.add("Now Showing");
+        //listDataHeader.add(jsonData.getKapitelName().get(0));
+
+        // Adding child data
+        List<String> top250 = new ArrayList<String>();
+        top250.add("The Shawshank Redemption");
+        top250.add("The Godfather");
+        top250.add("The Godfather: Part II");
+        top250.add("Pulp Fiction");
+        top250.add("The Good, the Bad and the Ugly");
+        top250.add("The Dark Knight");
+        top250.add("12 Angry Men");
+
+        List<String> nowShowing = new ArrayList<String>();
+        nowShowing.add("The Conjuring");
+        nowShowing.add("Despicable Me 2");
+        nowShowing.add("Turbo");
+        nowShowing.add("Grown Ups 2");
+        nowShowing.add("Red 2");
+        nowShowing.add("The Wolverine");
+
+        List<String> comingSoon = new ArrayList<String>();
+        comingSoon.add("2 Guns");
+        comingSoon.add("The Smurfs 2");
+        comingSoon.add("The Spectacular Now");
+        comingSoon.add("The Canyons");
+        comingSoon.add("Europa Report");
+
+        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), nowShowing);
+        listDataChild.put(listDataHeader.get(2), comingSoon);
     }
 
 }

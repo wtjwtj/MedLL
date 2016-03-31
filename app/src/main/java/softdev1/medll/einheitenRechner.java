6ccb539577c356ddb6a8cbe6133f78fc8b09777f
wtjwtj <imoutopls@gmail.com>
2016-03-31 16:08:06 +0200

    Update einheitenRechner.java

--- app/src/main/java/softdev1/medll/einheitenRechner.java
+++ app/src/main/java/softdev1/medll/einheitenRechner.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.content.Intent;
 
 import java.util.ArrayList;
 import java.util.List;
